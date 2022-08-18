dfc89b775ddd82594cbf4a7b3a5be8c883b5f5ff
Titus Mumo <titusmumo@gmail.com>
2022-08-18 12:21:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+//function expression called divide
+const divide = function(num1 = 2000, num2 = 100) {
+    return num1 / num2;
+}
+//arrow function called quare
+const square = (x = 2)=>{
+    return x ** 2;
+}
+//arrow function called add
+const add = (a, b)=> a + b;
